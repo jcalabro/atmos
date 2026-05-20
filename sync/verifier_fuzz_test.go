333406048db649da2e81a0caae2a23f9de03a2a1
jcalabro <jamesrcalabro@gmail.com>
2026-05-20 11:58:24 -0400

    sync: fuzz target for VerifyAndExpand

--- sync/verifier_fuzz_test.go
+++ sync/verifier_fuzz_test.go
@@ -1,13 +1,17 @@
 package sync_test
 
 import (
+	"context"
 	"errors"
 	"testing"
 
 	"github.com/jcalabro/atmos/api/comatproto"
 	"github.com/jcalabro/atmos/api/lextypes"
 	"github.com/jcalabro/atmos/cbor"
+	"github.com/jcalabro/atmos/identity"
+	"github.com/jcalabro/atmos/internal/testutil"
 	"github.com/jcalabro/atmos/sync"
+	"github.com/jcalabro/atmos/xrpc"
 	"github.com/jcalabro/gt"
 )
 
@@ -40,5 +44,36 @@ func FuzzInvertCommit(f *testing.F) {
 		if !errors.As(err, &ie) {
 			t.Fatalf("expected *InversionError or nil, got %T: %v", err, err)
 		}
+	})
+}
+
+// FuzzVerifyAndExpand drives random byte streams as the CAR diff into
+// a verifier. Asserts (a) no panics, (b) any returned error is one of
+// our typed errors or a wrapped infrastructure error.
+func FuzzVerifyAndExpand(f *testing.F) {
+	f.Add([]byte{0x00})
+	f.Add([]byte{0xff, 0xff, 0xff, 0xff})
+
+	dir := &identity.Directory{Resolver: testutil.NewTrackingResolver()}
+	v, err := sync.NewVerifier(sync.VerifierOptions{
+		SyncClient: sync.NewClient(sync.Options{Client: &xrpc.Client{Host: "https://nope.invalid"}}),
+		Directory:  dir,
+		ChainStore: sync.NewMemChainStore(),
+		Policy:     sync.PolicyError,
+	})
+	if err != nil {
+		f.Fatal(err)
+	}
+
+	f.Fuzz(func(t *testing.T, blocks []byte) {
+		commit := &comatproto.SyncSubscribeRepos_Commit{
+			Repo:   "did:plc:fuzzed",
+			Rev:    "3aaaaaaaaaaaa",
+			Blocks: blocks,
+			Commit: lextypes.LexCIDLink{Link: "bafyreigh2akiscaildc6dpyqhskdjkdg3hglmqgqsaftvjj5d3lqvazgha"},
+		}
+		_, err := v.VerifyAndExpand(context.Background(), commit, nil)
+		// Any returned error is acceptable as long as it isn't a panic.
+		_ = err
 	})
 }
